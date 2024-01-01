7771553cad5a33f16c0b1ffe34f257962028beec
IliaWithHat <rozhkoilia5533@gmail.com>
2024-01-01 10:10:44 +0200

    Fresh start.

---
+++ src/main/java/org/ilia/inventoryingapp/InventoryingAppApplication.java
@@ -0,0 +1,13 @@
+package org.ilia.inventoryingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ilia/inventoryingapp/InventoryingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilia.inventoryingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
