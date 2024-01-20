afa165b6eafb7385a09f4a3cde653c461493fb7d
IliaWithHat <rozhkoilia5533@gmail.com>
2024-01-20 14:10:19 +0200

    Add second level cache.

--- src/main/java/org/ilia/inventoryingapp/database/repository/InventoryRepository.java
+++ src/main/java/org/ilia/inventoryingapp/database/repository/InventoryRepository.java
@@ -1,14 +1,18 @@
 package org.ilia.inventoryingapp.database.repository;
 
+import jakarta.persistence.QueryHint;
 import org.ilia.inventoryingapp.database.entity.Inventory;
 import org.ilia.inventoryingapp.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+import static org.hibernate.annotations.QueryHints.CACHEABLE;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long>, QuerydslPredicateExecutor<Inventory>, ItemAndInventoryRepository {
@@ -17,5 +21,6 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long>, Que
     @Query("delete from Inventory i where i.user.id = :userId")
     void deleteInventoryByUserId(Integer userId);
 
+    @QueryHints(@QueryHint(name = CACHEABLE, value = "true"))
     Optional<Inventory> findInventoryByInventoryNumberAndUser(String inventoryNumber, User user);
 }
--- src/main/java/org/ilia/inventoryingapp/database/repository/ItemAndInventoryRepository.java
+++ src/main/java/org/ilia/inventoryingapp/database/repository/ItemAndInventoryRepository.java
@@ -1,14 +1,20 @@
 package org.ilia.inventoryingapp.database.repository;
 
 import com.querydsl.core.Tuple;
+import jakarta.persistence.QueryHint;
 import org.ilia.inventoryingapp.database.entity.Item;
 import org.ilia.inventoryingapp.filter.ItemFilter;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.hibernate.annotations.QueryHints.CACHEABLE;
 
 public interface ItemAndInventoryRepository {
 
+    @QueryHints(@QueryHint(name = CACHEABLE, value = "true"))
     Page<Item> findItemsThatWereNotInventoried(ItemFilter itemFilter, UserDetails userDetails, Integer pageNumber);
 
+    @QueryHints(@QueryHint(name = CACHEABLE, value = "true"))
     Page<Tuple> findItemsAndInventory(ItemFilter itemFilter, UserDetails userDetails, Integer pageNumber);
 }
