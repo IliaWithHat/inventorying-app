b74891970238cec8c3bebc8aa7f6e057c5531900
IliaWithHat <rozhkoilia5533@gmail.com>
2024-01-23 13:53:55 +0200

    Change font size

--- src/main/java/org/ilia/inventoryingapp/database/entity/Role.java
+++ src/main/java/org/ilia/inventoryingapp/database/entity/Role.java
@@ -2,7 +2,7 @@ package org.ilia.inventoryingapp.database.entity;
 
 import org.springframework.security.core.GrantedAuthority;
 
-//TODO ADMIN can create USER(can not create another USER), USER can create SLAVE(can do only inventorying);
+//TODO ADMIN can create USER can do only inventorying;
 public enum Role implements GrantedAuthority {
     ADMIN,
     USER;
