204c623f80216dce7ec1f791146bf1aa8cc61ac7
IliaWithHat <rozhkoilia5533@gmail.com>
2024-01-23 12:32:52 +0200

    Fix

--- src/main/java/org/ilia/inventoryingapp/http/controller/MainPageController.java
+++ src/main/java/org/ilia/inventoryingapp/http/controller/MainPageController.java
@@ -2,12 +2,13 @@ package org.ilia.inventoryingapp.http.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainPageController {
 
     @GetMapping("/")
     public String getMainPage() {
-        return "redirect:/items";
+        return "index";
     }
 }
