a080f4f0a401c2d4697e25dc8c4f9279ca7700b3
IliaWithHat <rozhkoilia5533@gmail.com>
2024-07-03 17:12:04 +0300

    Update docker-push.bat.

--- docker-push.bat
+++ docker-push.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set DOCKERHUB_USERNAME=iliawithhat
+set APP_NAME=inventorying-app
 
 echo.
 echo Building jar file
@@ -8,17 +9,19 @@ call ./gradlew.bat bootJar
 
 echo.
 echo Building Docker image
-docker build -t inventorying-app-image .
+docker build -t %APP_NAME% .
 
-docker tag inventorying-app-image:latest %DOCKERHUB_USERNAME%/inventorying-app:latest
+echo.
+echo Setting tag to image
+docker tag %APP_NAME%:latest %DOCKERHUB_USERNAME%/%APP_NAME%:latest
 
 echo.
 echo Pushing image to Docker Hub
-docker push %DOCKERHUB_USERNAME%/inventorying-app:latest
+docker push %DOCKERHUB_USERNAME%/%APP_NAME%:latest
 
 echo.
 echo Removing local image
-docker rmi inventorying-app-image
+docker rmi %APP_NAME%
 
 echo.
 echo Image pushed to Docker Hub successfully.
